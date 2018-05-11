e38a18bb95ee5024aacdb593b15c73e4a1d47718
Dr. Masroor Ehsan <masroore@gmail.com>
2018-05-11 21:18:06 +0200

    updated config for spark

--- config/app.php
+++ config/app.php
@@ -154,6 +154,9 @@ return [
         /*
          * Application Service Providers...
          */
+        Laravel\Spark\Providers\SparkServiceProvider::class,
+        App\Providers\SparkServiceProvider::class,
+        Laravel\Cashier\CashierServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
@@ -208,7 +211,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
 
 ];
