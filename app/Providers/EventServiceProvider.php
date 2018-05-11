e2947b852bd975c9c2abee38a2ddbab20eab0bc9
Dr. Masroor Ehsan <masroore@gmail.com>
2018-05-11 21:03:38 +0200

    minor refactoring

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -13,13 +12,69 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+        // User Related Events...
+        'Laravel\Spark\Events\Subscription\UserSubscribed' => [
+            'Laravel\Spark\Listeners\Subscription\UpdateActiveSubscription',
+            'Laravel\Spark\Listeners\Subscription\UpdateTrialEndingDate',
+        ],
+
+        'Laravel\Spark\Events\Profile\ContactInformationUpdated' => [
+            'Laravel\Spark\Listeners\Profile\UpdateContactInformationOnStripe',
+        ],
+
+        'Laravel\Spark\Events\PaymentMethod\VatIdUpdated' => [
+            'Laravel\Spark\Listeners\Subscription\UpdateTaxPercentageOnStripe',
+        ],
+
+        'Laravel\Spark\Events\PaymentMethod\BillingAddressUpdated' => [
+            'Laravel\Spark\Listeners\Subscription\UpdateTaxPercentageOnStripe',
+        ],
+
+        'Laravel\Spark\Events\Subscription\SubscriptionUpdated' => [
+            'Laravel\Spark\Listeners\Subscription\UpdateActiveSubscription',
+        ],
+
+        'Laravel\Spark\Events\Subscription\SubscriptionCancelled' => [
+            'Laravel\Spark\Listeners\Subscription\UpdateActiveSubscription',
+        ],
+
+        // Team Related Events...
+        'Laravel\Spark\Events\Teams\TeamCreated' => [
+            'Laravel\Spark\Listeners\Teams\UpdateOwnerSubscriptionQuantity',
+        ],
+
+        'Laravel\Spark\Events\Teams\TeamDeleted' => [
+            'Laravel\Spark\Listeners\Teams\UpdateOwnerSubscriptionQuantity',
+        ],
+
+        'Laravel\Spark\Events\Teams\TeamMemberAdded' => [
+            'Laravel\Spark\Listeners\Teams\UpdateTeamSubscriptionQuantity',
+        ],
+
+        'Laravel\Spark\Events\Teams\TeamMemberRemoved' => [
+            'Laravel\Spark\Listeners\Teams\UpdateTeamSubscriptionQuantity',
+        ],
+
+        'Laravel\Spark\Events\Teams\Subscription\TeamSubscribed' => [
+            'Laravel\Spark\Listeners\Teams\Subscription\UpdateActiveSubscription',
+            'Laravel\Spark\Listeners\Teams\Subscription\UpdateTrialEndingDate',
+        ],
+
+        'Laravel\Spark\Events\Teams\Subscription\SubscriptionUpdated' => [
+            'Laravel\Spark\Listeners\Teams\Subscription\UpdateActiveSubscription',
+        ],
+
+        'Laravel\Spark\Events\Teams\Subscription\SubscriptionCancelled' => [
+            'Laravel\Spark\Listeners\Teams\Subscription\UpdateActiveSubscription',
+        ],
+
+        'Laravel\Spark\Events\Teams\UserInvitedToTeam' => [
+            'Laravel\Spark\Listeners\Teams\CreateInvitationNotification',
         ],
     ];
 
     /**
-     * Register any events for your application.
+     * Register any other events for your application.
      *
      * @return void
      */
--- app/User.php
+++ app/User.php
@@ -2,28 +2,52 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Spark\CanJoinTeams;
+use Laravel\Spark\User as SparkUser;
 
-class User extends Authenticatable
+class User extends SparkUser
 {
-    use Notifiable;
-
+    use CanJoinTeams;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'authy_id',
+        'country_code',
+        'phone',
+        'two_factor_reset_code',
+        'card_brand',
+        'card_last_four',
+        'card_country',
+        'billing_address',
+        'billing_address_line_2',
+        'billing_city',
+        'billing_zip',
+        'billing_country',
+        'extra_billing_information',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'trial_ends_at' => 'datetime',
+        'uses_two_factor_auth' => 'boolean',
     ];
 }
